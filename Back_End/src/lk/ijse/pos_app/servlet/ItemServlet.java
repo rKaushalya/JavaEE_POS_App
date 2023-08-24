6c375895d4b1485dd24681f143a822fae377b445
rKaushalya <ravinduk56@gmail.com>
2023-08-24 23:29:48 +0530

    Create ItemServlet method

--- Back_End/src/lk/ijse/pos_app/servlet/ItemServlet.java
+++ Back_End/src/lk/ijse/pos_app/servlet/ItemServlet.java
@@ -1,4 +1,36 @@
 package lk.ijse.pos_app.servlet;
 
-public class ItemServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "")
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
